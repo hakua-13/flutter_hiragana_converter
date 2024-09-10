0a0af665e899c78a4a99ecdbea6482d414f57ac7
hakua <hakua1452@gmail.com>
2024-09-10 09:48:38 +0900

    画面切り替え、ひらがな変換処理の呼び出し修正

--- lib/input_form.dart
+++ lib/input_form.dart
@@ -1,17 +1,16 @@
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hiragana_conversion_app/data.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_hiragana_conversion_app/app_notifier_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class InputForm extends StatefulWidget {
+class InputForm extends ConsumerStatefulWidget {
   const InputForm({super.key});
 
   @override
-  _InputFormState createState() => _InputFormState();
+  ConsumerState<InputForm> createState() => _InputFormState();
 }
 
-class _InputFormState extends State<InputForm> {
+class _InputFormState extends ConsumerState<InputForm> {
   final _formKey = GlobalKey<FormState>();
   final _textEditingController = TextEditingController();
 
@@ -45,24 +44,12 @@ class _InputFormState extends State<InputForm> {
               if (!formState.validate()) {
                 return;
               }
-              final url = Uri.parse('https://labs.goo.ne.jp/api/hiragana');
-              final headers = {'Content-Type': 'application/json'};
-              final request = Request(
-                appId: const String.fromEnvironment('appId'),
-                sentence: _textEditingController.text,
-              );
+              final sentence = _textEditingController.text;
+              await ref
+                .read(appNotifierProvider.notifier)
+                .convert(sentence);
 
-              final result = await http.post(
-                url,
-                headers: headers,
-                body: jsonEncode(request.toJson()),
-              );
-
-              final response = Response.fromJson(
-                jsonDecode(result.body) as Map<String, Object?>,
-              );
-
-              debugPrint('変換結果: ${response.converted}');
+              // debugPrint('変換結果: ${response.converted}');
             },
             child: const Text('変換')
           )
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hiragana_conversion_app/app_notifier_provider.dart';
+import 'package:flutter_hiragana_conversion_app/app_state.dart';
+import 'package:flutter_hiragana_conversion_app/convert_result.dart';
 import 'package:flutter_hiragana_conversion_app/input_form.dart';
+import 'package:flutter_hiragana_conversion_app/loading_indicator.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: MyApp())
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -21,17 +28,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomeScreen extends StatelessWidget {
+class MyHomeScreen extends ConsumerWidget {
   const MyHomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appState = ref.watch(appNotifierProvider);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Hiragana Converter')
       ),
-      body: const InputForm(),
+      body: switch(appState) {
+        Loading() => const LoadingIndicator(),
+        Input() => const InputForm(),
+        Data(sentence: final sentence) => ConvertResult(sentence: sentence,)
+      },
     );
   }
 }
